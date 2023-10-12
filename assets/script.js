14b9a3718a10e58d1fcf558048eb59f88d8b394a
danou <danouhamou@gmail.com>
2023-10-12 14:23:38 +0200

    debut de js

--- assets/script.js
+++ assets/script.js
@@ -18,50 +18,6 @@ const slides = [
 	},
   ];
   
-  let position = 0; 
-  const numberOfSlide = slides.length;
-  
-  createCaroussel(position);
-  createDots()
-  updateDot()
-  
 
-
-  const left = document.querySelector('.arrow_left');
-  const right = document.querySelector('.arrow_right');
-  
-  
-  
-  
-  left.addEventListener("click",function(){
-	if (position == 0){
-		position = numberOfSlide - 1;
-	}
-	else {
-		position --;
-	}
-	createCaroussel(position);
-
-  });
-
-  right.addEventListener("click", function(){
-	if (position = numberOfSlide - 1;) {
-		position = 0;
-	}
-	else{
-		position ++;
-	}
-	createCaroussel(position);
-  }); 
-
-
-  function createDots(){
-	const dots = document.querySelector(".dot");
-	for(let index = 0; index < slides.length; index++){
-		 // Pour chaque element dans la boucle je vais créer un dot
-		 const dot= document.createElement("div")
-		 dot.setAttribute("class", "dot");
-	}
-  }
-
-
+  let currentIndex = 0;
+  const bannerImg = document.querySelector('.banner-img')
