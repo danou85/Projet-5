07dc3130d8477897a5943f319c1fc3204adfc348
danou <danouhamou@gmail.com>
2023-10-15 14:32:51 +0200

    projet fini

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,71 @@
 const slides = [
-	{
-	  image: "slide1.jpg",
-	  tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
-	},
-	{
-	  image: "slide2.jpg",
-	  tagLine:
-		"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-	},
-	{
-	  image: "slide3.jpg",
-	  tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-	},
-	{
-	  image: "slide4.png",
-	  tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-	},
-  ];
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
+
+// Initialisation de l'indice de la diapositive actuelle
+let currentIndex = 0;
+
+// Sélection des éléments DOM nécessaires
+const bannerImg = document.querySelector(".banner-img"); // Sélection de l'image du carrousel
+const arrowLeft = document.querySelector(".arrow_left"); // Sélection du bouton gauche
+const arrowRight = document.querySelector(".arrow_right"); // Sélection du bouton droit
+const dots = document.querySelectorAll(".dot"); // Sélectionnez tous les points indicateurs
+
+function updateDots(indexSelected) {
+  dots.forEach((dot, indexDot) => {
+    if (indexDot === indexSelected){
+      dot.classList.add("dot_selected");
+    } else {
+      dot.classList.remove("dot_selected");
+    }
+  });
+}
+
+function updateCarousel(indexSelected, direction) {
+  if (indexSelected === -1 && direction === "left") {
+    currentIndex = slides.length - 1;
+  } else if (indexSelected === slides.length && direction === "right") {
+    currentIndex = 0;
+  }
   
+  const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
+  bannerImg.src = imagePath;
+  bannerImg.alt = `Slide ${currentIndex + 1}`;
+  
+  const tagLine = slides[currentIndex].tagLine;
+  document.querySelector('p').innerHTML = tagLine;
+}
 
-  let currentIndex = 0;
-  const bannerImg = document.querySelector('.banner-img')
-  const arrowLeft = document.querySelector('.arrow_left')
-  const arrowRight = document.querySelector('.arrow_right')
-  const dots = document.querySelector('dot')
+// Écouteur d'événement pour le bouton de gauche
+arrowLeft.addEventListener('click', () => {
+  currentIndex -= 1;
+  updateCarousel(currentIndex, 'left'); // Appelle la fonction de mise à jour pour la diapositive précédente
+  updateDots(currentIndex); // Met à jour les indicateurs de point
+});
 
+// Écouteur d'événement pour le bouton de droite
+arrowRight.addEventListener('click', () => {
+  currentIndex += 1;
+  updateCarousel(currentIndex, 'right'); // Appelle la fonction de mise à jour pour la diapositive suivante
+  updateDots(currentIndex); // Met à jour les indicateurs de point
+});
+
+// Initialisation du carrousel et des indicateurs de point lors du démarrage
+updateCarousel(currentIndex, 'démarrage');
+updateDots(currentIndex);
