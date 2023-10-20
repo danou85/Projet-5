e5323e7ecdc0e8889c70359faddac37ba64dae16
danou <danouhamou@gmail.com>
2023-10-20 15:18:20 +0200

    projet fini

--- assets/script.js
+++ assets/script.js
@@ -20,17 +20,14 @@ const slides = [
 
 let currentIndex = 0;
 
-const bannerImg = document.querySelector(".banner-img")
-const arrowLeft = document.querySelector(".arrow_left")
-const arrowRight = document.querySelector(".arrow_right")
-const dots = document.querySelectorAll(".dot")
-
-
-
+const bannerImg = document.querySelector(".banner-img");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const dots = document.querySelectorAll(".dot");
 
 function updateDots(indexSelected) {
   dots.forEach((dot, indexDot) => {
-    if (indexDot === indexSelected){
+    if (indexDot === indexSelected) {
       dot.classList.add("dot_selected");
     } else {
       dot.classList.remove("dot_selected");
@@ -39,42 +36,36 @@ function updateDots(indexSelected) {
 }
 
 function updateCarousel(indexSelected, direction) {
-  if (indexSelected === -1  && direction === "left"){
-    currentIndex = slides.length -1;
-  }
-  else if (indexSelected === slides.length && direction === "right"){
+  if (indexSelected === -1 && direction === "left") {
+    currentIndex = slides.length - 1;
+  } else if (indexSelected === slides.length && direction === "right") {
     currentIndex = 0;
   }
-  
+
   const imagePath = `assets/images/slideshow/${slides[currentIndex].image}`;
   bannerImg.src = imagePath;
   bannerImg.alt = `Slide ${currentIndex + 1}`;
-  
+
   const tagLine = slides[currentIndex].tagLine;
-  document.querySelector('p').innerHTML = tagLine;
+  document.querySelector("p").innerHTML = tagLine;
 }
 
 // Écouteur d'événement pour le bouton de gauche
 
-
-arrowLeft.addEventListener ('click',() =>{
+arrowLeft.addEventListener("click", () => {
   currentIndex -= 1;
-  updateCarousel(currentIndex, 'left');// Appelle la fonction de mise à jour pour la diapositive précédente
-  updateDots(currentIndex);// Met à jour les indicateurs de point
-});
-
-
-// Écouteur d'événement pour le bouton de droite
-
-
-arrowRight.addEventListener ('click',() =>{
-  currentIndex += 1;
-  updateCarousel(currentIndex, 'right'); // Appelle la fonction de mise à jour pour la diapositive suivante
+  updateCarousel(currentIndex, "left"); // Appelle la fonction de mise à jour pour la diapositive précédente
   updateDots(currentIndex); // Met à jour les indicateurs de point
 });
 
+// Écouteur d'événement pour le bouton de droite
+
+arrowRight.addEventListener("click", () => {
+  currentIndex += 1;
+  updateCarousel(currentIndex, "right"); // Appelle la fonction de mise à jour pour la diapositive suivante
+  updateDots(currentIndex); // Met à jour les indicateurs de point
+});
 
 // Initialisation du carrousel et des indicateurs de point lors du démarrage
-updateCarousel(currentIndex, 'démarrage');
+updateCarousel(currentIndex, "démarrage");
 updateDots(currentIndex);
-
